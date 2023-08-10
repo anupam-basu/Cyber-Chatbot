8851350ea250ba87a8bb87ec4f323b54d385cac2
anupam-basu <anupambasu@anupams-mbp.mynetworksettings.com>
2023-08-09 21:31:42 -0400

    cleaning files

--- modules/file_upload/file_upload_final.js
+++ modules/file_upload/file_upload_final.js
@@ -1,5 +1,4 @@
 var SLACK_BOT_TOKEN= SLACK.BOT.TOKEN;
-var APPLICATION_TOKEN= APPLICATION.TOKEN;
 var AWS_S3_BUCKET= AWS.S3.BUCKET;
 
 var RtmClient = require('@slack/client').RtmClient;
@@ -16,54 +15,53 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
         console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 });
 
-rtm.on(RTM_EVENTS.FILE_SHARED, function handleUpload(file){
+rtm.on(RTM_EVENTS.FILE_SHARED, function handleUpload(file) {
 
         var token= SLACK_BOT_TOKEN;
         var file_id=file.file.id;
         // here we a request is make to listen for file upload for that token
         var request = fetch("https://slack.com/api/files.info?token="+token+"&file="+file_id+"&pretty=1").then( function(response) {
-        return response.json();
-}).then(function(json){
-                    console.log (json.file.url_private_download);
-                    var file_name=String(json.file.name);
-                    var url=String(json.file.url_private_download);
-                    var path=url.substring(23,url.length);
-                    //EXTRACTING THE unique url directory
-                    console.log(path);
-                    var options = {
-                        "method": "GET",
-                        "hostname"  : "files.slack.com",
-                        "path":path,
-                        "rejectUnauthorized": "false",
-                        "headers": {
-                        "Authorization": "Bearer "+token
-                    }};
-        
-                    var req = https.request(options, function(res) {
-                    res.on('data', function(d) {
-                    var s3 = new AWS.S3({accessKeyId: env.AWS_ID, secretAccessKey:env.AWS_KEY});
-                    s3 = new AWS.S3({apiVersion: '2006-03-01'});
-                    s3.putObject({
-                        Bucket: AWS_S3_BUCKET,
-                        Key    : json.file.user+"_"+file_name,
-                        Body: d
-                    }, function (err) {
-                    if (err) 
-                       console.log(err);
-                    else 
-                       console.log("Success File was uploaded");
-                
-                    });
-                    s3.putObject({
-                        Bucket: AWS_S3_BUCKET,
-                        Key    : json.file.user+"_chatbot",
-                        Body: d
-                    }, function (err) {
-                    if (err) 
-                           console.log(err);
-                        else 
-                           console.log("Success File was uploaded");
-                    });
+            return response.json();
+        }).then(function(json) {
+            console.log (json.file.url_private_download);
+            var file_name=String(json.file.name);
+            var url=String(json.file.url_private_download);
+            var path=url.substring(23,url.length);
+            //EXTRACTING THE unique url directory
+            console.log(path);
+            var options = {
+                "method": "GET",
+                "hostname"  : "files.slack.com",
+                "path":path,
+                "rejectUnauthorized": "false",
+                "headers": {
+                "Authorization": "Bearer "+token
+            }};
+
+            var req = https.request(options, function(res) {
+            res.on('data', function(d) {
+            var s3 = new AWS.S3({accessKeyId: env.AWS_ID, secretAccessKey:env.AWS_KEY});
+            s3 = new AWS.S3({apiVersion: '2006-03-01'});
+            s3.putObject({
+                Bucket: AWS_S3_BUCKET,
+                Key   : json.file.user+"_"+file_name,
+                Body  : d
+            }, function (err) {
+            if (err) 
+                console.log(err);
+            else 
+                console.log("Success File was uploaded");
+            });
+            s3.putObject({
+                Bucket: AWS_S3_BUCKET,
+                Key    : json.file.user+"_chatbot",
+                Body: d
+            }, function (err) {
+            if (err) 
+                console.log(err);
+            else 
+                console.log("Success File was uploaded");
+            });
             });
         
         });
